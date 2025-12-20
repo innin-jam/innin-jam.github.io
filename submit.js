c93a5dabc94094a8a5989d523cfd1c205a482fa4
innin-jam <innin.jam@gmail.com>
2025-12-20 16:42:44 +0100

    added input functionality

---
+++ submit.js
@@ -0,0 +1,20 @@
+function submit(valid, link, messageFail="incorrect") {
+  let input = generateHash(document.getElementById("pwd").value);
+  // location.pathname = input;
+  if (input == valid) {
+    document.getElementById("validity").innerHTML = "correct";
+    location.pathname = link;
+  }
+  else {
+    document.getElementById("validity").innerHTML = messageFail;
+  }
+}
+
+const generateHash = (string) => {
+  let hash = 0;
+  for (const char of string) {
+    hash = (hash << 5) - hash + char.charCodeAt(0);
+    hash |= 0; // Constrain to 32bit integer
+  }
+  return hash;
+};
